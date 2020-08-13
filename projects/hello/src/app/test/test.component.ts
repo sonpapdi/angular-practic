bcea0b8a588488cf48feebfb5cffcc3ec260f54a
sonpapdi <snehalnakate4444@gamil.com>
2020-08-13 12:23:19 -0500

    youtub video

---
+++ projects/hello/src/app/test/test.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template:`
+  <h2>
+  welcome {{name}}
+  <h2>
+  hiiii {{string}}
+  ok {{add}}
+  <h2>{{2+2}}
+  </h2>
+  <h2>{{"pooja" + "nakate"}}</h2>
+  <h2> {{name.length}}</h2>
+  <h2>{{name.toUpperCase()}}</h2>
+  <h2>{{greetUser()}}</h2>
+`,
+  styles: []
+})
+export class TestComponent implements OnInit {
+
+  public name= "pooja";
+  public string ="pooja"+ "mau"
+ public add = 2+3
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  greetUser(){
+    
+     return "Hello" + this.name;
+  }
+
+}
