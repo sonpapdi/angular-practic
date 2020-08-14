f6cfec491baca43566ef5ce8ef5c5dde009c1101
sonpapdi <snehalnakate4444@gamil.com>
2020-08-14 12:07:32 -0500

    youtub video

--- projects/hello/src/app/test/test.component.ts
+++ projects/hello/src/app/test/test.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { InterpolationConfig } from '@angular/compiler';
-import { getCurrencySymbol } from '@angular/common';
+// import { InterpolationConfig } from '@angular/compiler';
+// import { getCurrencySymbol } from '@angular/common';
 
 @Component({
   selector: 'app-test',
@@ -8,30 +8,26 @@ import { getCurrencySymbol } from '@angular/common';
   <h2>
   welcome {{name}}
   </h2>
-  // style binding
-
-  <h2>  style binding</h2>
-  <h2 [style.color]=" hasError ? 'red' :'green'"> style binding </h2>
-
-<h2 [style.color]="higtlightcolor">style binding 2</h2>
-
+ <button (click)="onclick()">Greet</button>
+ {{greeting}}
 `,
 styles:[
-`
-`
+
 ]
 })
 export class TestComponent implements OnInit {
 
 public name ="codevolution";
-public hasError = true;
-public isSpecial = true;
-public higtlightcolor ="orange"
+public greeting =""
 
 
   constructor() { }
 
   ngOnInit() {
+  }
+  onclick(){
+    console.log('welcome to codekul')
+    this.greeting ="welcome"
   }
 
 }
