e66be069ee0023374c3283eb02ac4f9397847abc
sonpapdi <snehalnakate4444@gamil.com>
2020-08-14 11:51:55 -0500

    youtub video

--- projects/hello/src/app/test/test.component.ts
+++ projects/hello/src/app/test/test.component.ts
@@ -10,12 +10,10 @@ import { getCurrencySymbol } from '@angular/common';
   </h2>
   // style binding
 
-
-  
   <h2>  style binding</h2>
   <h2 [style.color]=" hasError ? 'red' :'green'"> style binding </h2>
 
-
+<h2 [style.color]="higtlightcolor">style binding 2</h2>
 
 `,
 styles:[
@@ -28,6 +26,7 @@ export class TestComponent implements OnInit {
 public name ="codevolution";
 public hasError = true;
 public isSpecial = true;
+public higtlightcolor ="orange"
 
 
   constructor() { }
