9de0693a67c110674c6410f0366f43f0b3a6d0a9
sonpapdi <snehalnakate4444@gamil.com>
2020-08-16 03:20:51 -0500

    Template reffrance varable

--- projects/hello/src/app/test/test.component.ts
+++ projects/hello/src/app/test/test.component.ts
@@ -8,31 +8,23 @@ import { Component, OnInit } from '@angular/core';
   <h2>
   welcome {{name}}
   </h2>
-
-  Event Binding //////
-
- <button (click)="onclick()">Greet</button>
- {{greeting}}
-<button (click)="onclick($event)"></button>// $event to display DOM 
-
+  <input #myinput type="text">
+   <button (click)="logmessage(myinput.value)">log</button>
+  
 `,
-styles:[
-
-]
+styles:[]
 })
 export class TestComponent implements OnInit {
 
-public name ="codevolution";
-public greeting =""
-
+public name ="codevalution"
 
   constructor() { }
 
   ngOnInit() {
   }
-  onclick( Event){
-    console.log(event)
-    this.greeting =event.type;// click event diplay
-  }
 
+logmessage(value){
+  console.log(value);
+
+}
 }
