beb93e0e9e37d20c61ae70518f02f1739f390bcd
sonpapdi <snehalnakate4444@gamil.com>
2020-08-16 05:10:17 -0500

    ngFor....

--- projects/hello/src/app/test/test.component.ts
+++ projects/hello/src/app/test/test.component.ts
@@ -5,20 +5,17 @@ import { getCurrencySymbol } from '@angular/common';
 @Component({
   selector: 'app-test',
   template:`
-
-<div [ngSwitch]="color">
-<div *ngSwitchCase ="'red'">you picked red color</div>
-<div *ngSwitchCase ="'yellow'"> you picked yellow color</div>
-<div *ngSwitchCase ="'green'"> you picked green color</div>
-<div *ngSwitchDefault> pick again</div>
+ 
+<div *ngFor="let color of colors; index as i">
+<h2> {{i}} {{color}}</h2>
 </div>
-
+ 
 `,
 styles:[]
 })
 export class TestComponent implements OnInit {
-public color ="black";
-  
+
+  public colors =["red","blue","green","yellow"];
   constructor() { }
 
   ngOnInit() {
